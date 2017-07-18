ba2f6f8fd02fd0c88d78ac414f84f8bf0cfdcab1
msant <marcosantana@msn.com>
2017-07-17 23:18:32 -0300

    implemented modal example

--- app/biz/cliente/cliente.list.controller.js
+++ app/biz/cliente/cliente.list.controller.js
@@ -5,17 +5,48 @@
     .module('biz.cliente')
     .controller('BizClienteListController', BizClienteListController);
 
-  BizClienteListController.$inject = ['coreModal', 'bizClienteService'];
+  BizClienteListController.$inject = ['$scope', 'coreModal', 'bizClienteService'];
   /* @ngInject */
-  function BizClienteListController(coreModal, bizClienteService) {
+  function BizClienteListController($scope, coreModal, bizClienteService) {
     var vm = this;
     vm.title = 'Clientes';
 
     activate();
 
+    vm.open = function openItem (item) {
+        var modal = {
+          options : {
+            controller : 'BizClienteDetailsController as vm', 
+            templateUrl : 'app/biz/cliente/cliente.details.html'
+          }, 
+          data : item
+        };           
+
+      coreModal.subscribe($scope, function (arg1, arg2) {
+        console.log('closed with item update'); 
+        //console.log()
+      });
+
+
+      var instance = coreModal.show(modal); 
+
+       instance.result.then(function(){
+        //Get triggers when modal is closed
+        console.log('modal closed');
+        //never triggers        
+       }, function(){
+        console.log('modal dismissed');
+        console.log(item);
+        //gets triggers when modal is dismissed.
+       });
+    }
+
     function activate() {
       vm.list = bizClienteService.getList(); 
-      console.log('Activated Admin View');
+
+
+
+      //console.log('Activated Admin View');
     }
   }
 })();
--- app/core/modal/core.modal.service.js
+++ app/core/modal/core.modal.service.js
@@ -36,6 +36,8 @@
 					}
 				}
 			});
+
+			return svc.modalInstance; 
 		};
 	}
 }());
