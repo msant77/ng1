ac1f2db57b2ba76c111cb7a8e680b3aa5a0d1cc8
Marcos Moritz <marcosmoritz@gmail.com>
2017-09-13 02:31:58 -0300

    Setup karma and implement first spec

---
+++ app/core/login/login.service.spec.js
@@ -0,0 +1,225 @@
+describe('login.service', function () {
+
+  // declare globals
+  var LoginService;
+  var $q, $window, $firebaseAuth, localdb, config, $rootScope;
+
+  // mock data
+  var userA = {
+    displayName: 'John Doe',
+    photoUrl: 'http://mockphoto.com/john-doe.jpg',
+    email: 'johndoe@firebase.com',
+    password: '123',
+    uid: 'john-doe-uid',
+    credentials: { accessToken: '123abc' }
+  };
+
+  var userB = {
+    displayName: 'Jane Doe',
+    photoUrl: 'http://mockphoto.com/jane-doe.jpg',
+    email: 'janedoe@firebase.com',
+    password: '123',
+    uid: 'jane-doe-uid'
+  }
+
+  var invalidPasswordResponse = {
+    code: 'auth/wrong-password',
+		message: 'mock message: invalid password',
+		email: userA.email,
+		credential: {}
+  };
+
+  var userNotFoundResponse = {
+    code: "auth/user-not-found",
+		message: 'user not found, create a new user',
+		email: userB.email,
+		credential: {}
+  };
+
+  beforeEach(function () {
+    module('firebase');
+    module(function($provide){
+      $provide.value('$firebaseAuth', function() {
+
+        function $signInWithPopup(provider) {
+          return $q(function mockResolveGoogle(resolve, reject) {
+            resolve({
+              user: userA,
+              credential: userA.credentials
+            });
+          });
+        }
+        
+        function $signInWithEmailAndPassword(email, password) {
+          if (email === userA.email) {
+            if (password === userA.password) {
+
+              // email and password correct
+              return $q( function resolveEmail(resolve, reject) {
+                resolve({
+                  uid: userA.uid
+                });
+              });
+            } else {
+
+              // wrong password
+              return $q(function mockRejectEmail(resolve, reject) {
+                reject(invalidPasswordResponse);
+              });
+            }
+          } else {
+            
+            // wrong email (new user)
+            return $q(function mockRejectUnregisteredEmail(resolve, reject) {
+              reject(userNotFoundResponse);
+            });
+          }
+        }
+        
+        function $createUserWithEmailAndPassword(email, password) {
+          return $q(function mockResolveNewEmail(resolve, reject) {
+            resolve({
+              uid: userB.uid
+            });
+          });
+        }
+
+        return {
+          $signInWithPopup: $signInWithPopup,
+          $signInWithEmailAndPassword: $signInWithEmailAndPassword,
+          $createUserWithEmailAndPassword: $createUserWithEmailAndPassword          
+        };
+      });
+    });
+  });
+  
+	beforeEach(function() {
+    module('core.start');
+    
+    module(function($provide){
+
+      var data = readJSON('app/core/start/settings.json');
+      
+			$provide.constant('config', data.config);
+      $provide.constant('appRoutes', data.routes);
+      
+    });
+
+    module('app.core');
+
+    module(function ($provide) {
+      $provide.factory('localdb', function($localStorage, config) {
+        var mockValue = 'mock-value';
+        function get(key, userSensitive) {
+          return mockValue;
+        }
+        function set(key, value, userSensitive) {
+          mockValue = value;
+        }
+        return{
+          get: get,
+          set: set
+        };
+      });
+    });
+	});
+  
+	beforeEach(inject(function(_LoginService_, _$firebaseAuth_, _localdb_, _$q_, _$window_, _config_, _$rootScope_) {
+		LoginService = _LoginService_;
+    $firebaseAuth = _$firebaseAuth_;
+    localdb = _localdb_;
+    $q = _$q_;
+    $window = _$window_;
+    config = _config_;
+    $rootScope = _$rootScope_;
+	}));
+	
+	it('should work', function() {
+		expect(LoginService).toBeDefined();
+	});
+
+	describe('.loginGoogle', function() {
+		it('should be able to resolve', function() {
+      var promise = LoginService.loginGoogle();
+
+      promise.then(
+        function(response) {
+          expect(response.name).toEqual(userA.displayName);
+        });
+
+      $rootScope.$digest();
+		});
+	});
+
+  describe('.loginEmail', function() {
+		it('should be able to resolve', function() {
+      var promise = LoginService.loginEmail(userA.email, userA.password);
+
+      promise.then(
+        function(response) {
+          expect(response.name).toEqual('guest');
+        });
+
+      $rootScope.$digest();
+		});
+
+		it('should be able to reject', function() {
+      var promise = LoginService.loginEmail(userA.email, 'wrong-password');
+
+      promise
+        .then(function (response) {
+          expect(false).toEqual(true);
+        })
+        .catch(
+          function(error) {
+            expect(error).toBe('mock message: invalid password');
+          });
+
+      $rootScope.$digest();
+		});
+
+    it('should be able to create a new user', function() {
+      var promise = LoginService.loginEmail(userB.email, userB.password);
+
+      promise
+        .then(function (response) {
+          expect(response.token).toEqual('jane-doe-uid');
+        })
+        .catch(function () {
+          expect(false).toEqual(true);
+        });
+      $rootScope.$digest();
+		});
+	});
+  
+  describe('.logout', function() {
+
+    it('should exist', function() {
+      expect(LoginService.logout).toBeDefined();
+		});
+    
+    it('should call localdb.set', function() {
+      spyOn(LoginService, 'logout').and.callThrough();
+
+      expect(localdb.get()).toEqual('mock-value');
+      
+      LoginService.logout();
+      expect(LoginService.logout).toHaveBeenCalled();
+      expect(localdb.get()).toEqual('');
+		});
+	});
+
+  describe('.islogged', function() {
+
+    it('should exist', function() {
+      expect(LoginService.islogged).toBeDefined();
+		});
+
+    it('should respond to mocked localdb data changes', function () {
+      expect(LoginService.islogged()).toEqual(true);
+
+      LoginService.logout();
+      expect(LoginService.islogged()).toEqual(undefined);
+    });
+	})
+});
---
+++ karma.conf.js
@@ -0,0 +1,123 @@
+// Karma configuration
+// Generated on Fri Sep 08 2017 14:09:34 GMT-0300 (BRT)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-babel-preprocessor'
+    ],
+    
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'app/**/*.js': ['babel']
+    },
+
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015'],
+        sourceMap: 'inline'
+      },
+      filename: function (file) {
+        return file.originalPath.replace(/\.js$/, '.es5.js');
+      },
+      sourceFileName: function (file) {
+        return file.originalPath;
+      }
+    },
+
+    // list of files / patterns to load in the browser
+    files: [
+    	'js/src/angular/angular.min.js',
+	    'js/src/angular-resource/angular-resource.min.js',
+	    'js/src/angular-ui-router/release/angular-ui-router.min.js',
+	    'js/src/angular-sanitize/angular-sanitize.min.js',
+	    'js/src/ngstorage/ngStorage.min.js',
+	    'js/src/angular-messages/angular-messages.min.js',
+	    'js/src/angular-aria/angular-aria.min.js',
+	    'js/src/angular-animate/angular-animate.min.js',
+	    'js/src/angular-material/angular-material.min.js',
+      'js/src/firebase/firebase.js',
+	    'js/src/angularfire/dist/angularfire.min.js',
+
+      // dev modules in node_modules
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/karma-read-json/karma-read-json.js',
+
+      // modules first
+      // app.core dependencies
+      'app/core/app.core.module.js',
+      // 'app/core/shell/core.shell.module.js',
+      'app/core/start/core.start.module.js',
+      'app/biz/services/biz.services.module.js',
+
+      'app/core/login/login.service.js',
+      // 'app/core/start/core.start.module.js',
+      
+      // 'app/core/shell/core.shell.module.js',
+      // 'app/biz/app.biz.module.js',
+      'app/biz/services/biz.services.module.js',
+      'app/biz/cliente/cliente.module.js',
+      // 'app/biz/user/user.module.js',
+      
+      'app/**/*.js',
+      'app/**/*.spec.js',
+
+      {pattern: './**/*.json', included:  false}
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    //  available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun:   false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+    proxies: {
+      '/app/core/start/settings.json':'http://localhost:9876/app/core/start/settings.json'
+    }
+  })
+}
